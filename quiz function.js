c45f368c6c27d7c47fcadd2cdbd9f4cde774c42c
sumedha-20 <bhusumi@gmail.com>
2023-12-27 18:11:33 +0530

    first commit

---
+++ quiz function.js
@@ -0,0 +1,35 @@
+let currentQuiz = 0;
+let score = 0;
+const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+};
+const getSelected = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+        if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+};
+const loadQuiz = () => {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+};
+loadQuiz();
+submitButton.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) score++;
+        currentQuiz++;
+        if (currentQuiz < quizData.length) loadQuiz();
+        else {
+            quiz.innerHTML = ` <h2>You answered ${score}/${quizData.length} questions correctly</h2> 
+                <button onclick="history.go(0)">Play Again</button> `
+                // location.reload() won't work in CodePen for security reasons; } } });
+        }
+    }
+});
